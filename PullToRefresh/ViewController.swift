074c41001836d2a685437ef8f8e9d4b358015f6e
Alexander Selivanov <alx.selivanov@gmail.com>
2019-04-16 20:12:24 +0700

    Adds pullToRefresh

--- PullToRefresh/ViewController.swift
+++ PullToRefresh/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private var refreshControl: UIRefreshControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        refreshControl = UIRefreshControl()
+        
+        let action = #selector(onPullToRefresh(_:))
+        refreshControl.addTarget(self, action: action, for: .valueChanged)
+        
+        tableView.refreshControl = refreshControl
     }
 
-
+    @IBAction func onPullToRefresh(_ sender: UIRefreshControl) {
+        
+        print(#function)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) { [weak sender] in
+            sender?.endRefreshing()
+        }
+    }
 }
 
